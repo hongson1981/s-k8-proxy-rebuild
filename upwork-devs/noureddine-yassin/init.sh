b720805dafa46ecde2342d8529e7b2f2125115cd
NourEddineX <noureddinex@protonmail.com>
2020-09-30 23:52:29 +0200

    fixes

--- upwork-devs/noureddine-yassin/init.sh
+++ upwork-devs/noureddine-yassin/init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-export CONFIG_FILE=cluster/hosts.yaml
-git clone https://github.com/kubernetes-sigs/kubespray
+declare -a IPS=${IPS[@]}
+if [ ! -d kubespray ] ; then
+    DIR=$(curl -s https://api.github.com/repos/kubernetes-sigs/kubespray/releases/latest | grep "tarball_url" | cut -d : -f 2,3 | \
+    tr -d \" | tr -d "," | wget -qi - -O- | tar xzv | tac | tail -n 1 ) # Download latest Kubespray release, extract it and move to "kubespray"
+    mv $DIR kubespray
+    cp -f kubespray/ansible.cfg . 
+fi 
 pip3 install -r kubespray/requirements.txt
-python3 kubespray/contrib/inventory_builder/inventory.py $IP
+CONFIG_FILE=cluster/hosts.yaml python3 kubespray/contrib/inventory_builder/inventory.py $IP
